231e2addd79259de11c82acc5635da7fb0c90202
Imran <128302897+imranmiatech@users.noreply.github.com>
2025-12-01 12:00:16 +0600

    Update List.jsx

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -9,7 +9,7 @@ const List = ({token}) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const fetchList = async () => {
         try {
-          const response = await axios.get(`${backendUrl}/api/product/list`)
+          const response = await axios.get("https://agro-backend-chi.vercel.app/api/product/list")
           if(response.data.success){
             setList(response.data.products)
           } else{
